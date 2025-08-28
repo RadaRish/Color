b89b0e5d9cd7b1898445465309c47a1dddc018c9
RadaRish <107124341+RadaRish@users.noreply.github.com>
2025-08-29 00:23:01 +0300

    upload

---
+++ core/project-manager.js
@@ -0,0 +1,4 @@
+// Переименовано с project_manager.js
+import ProjectManager from './project_manager.js';
+export default ProjectManager;
+
---
+++ core/viewer-manager.js
@@ -0,0 +1,4 @@
+// Переименовано с viewer_manager.js
+import ViewerManager from './viewer_manager.js';
+export default ViewerManager;
+
